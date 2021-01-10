1412a0b3f5e9302e8e1784e98a39e2ea4ede4ab5
Mo Sho <mohammed.sholaja@gmail.com>
2021-01-10 10:52:14 -0600

    json edit

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
     
-
 class AddPhoto extends Component {
 
     constructor() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App'
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter } from 'react-router-dom'
 import './styles/stylesheet.css'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux';
